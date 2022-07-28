4af1aa6e3d7edb09f476cd8caa02dfacca2f928b
Marcos Morais <marcostmorais@gmail.com>
2022-07-28 17:19:35 -0300

    chore: added platforms and default localization

--- Package.swift
+++ Package.swift
@@ -5,6 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "TypeEffectivenessKit",
+    defaultLocalization: "en",
+    platforms: [
+      .iOS(.v14),
+      .macOS(.v11),
+      .watchOS(.v7),
+      .tvOS(.v14)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
