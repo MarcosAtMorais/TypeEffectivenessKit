cfdd0149d1557fbd317c4684213410e85089c8d6
Marcos Morais <marcostmorais@gmail.com>
2022-08-02 13:28:41 -0300

    feat: Steel

--- Sources/TypeEffectivenessKit/Model/Types/Computed/SteelType.swift
+++ Sources/TypeEffectivenessKit/Model/Types/Computed/SteelType.swift
@@ -7,6 +7,30 @@
 
 import Foundation
 
+enum Steel: TypeWeaknessable {
+    
+    var type: PokemonType {
+        return .steel
+    }
+    
+    var noDamageFrom: [PokemonType] {
+        return [.poison]
+    }
+    
+    var halfDamageFrom: [PokemonType] {
+        return [.normal, .flying, .rock, .bug, .steel, .grass, .psychic, .ice, .dragon, .fairy]
+    }
+    
+    var normalDamageFrom: [PokemonType] {
+        return [.ghost, .water, .electric, .dark]
+    }
+    
+    var doubleDamageFrom: [PokemonType] {
+        return [.fighting, .ground, .fire]
+    }
+    
+}
+
 enum SteelType: TypeConformable {
     
     case secondType(type: PokemonType)
