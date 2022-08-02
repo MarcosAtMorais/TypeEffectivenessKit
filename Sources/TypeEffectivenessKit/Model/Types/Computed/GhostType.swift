94156e45971ef1e3670c4f2393d263ea5236ca00
Marcos Morais <marcostmorais@gmail.com>
2022-08-02 13:26:42 -0300

    feat: Ghost

--- Sources/TypeEffectivenessKit/Model/Types/Computed/GhostType.swift
+++ Sources/TypeEffectivenessKit/Model/Types/Computed/GhostType.swift
@@ -7,6 +7,30 @@
 
 import Foundation
 
+enum Ghost: TypeWeaknessable {
+    
+    var type: PokemonType {
+        return .ghost
+    }
+    
+    var noDamageFrom: [PokemonType] {
+        return []
+    }
+    
+    var halfDamageFrom: [PokemonType] {
+        return [.fighting, .ground, .grass]
+    }
+    
+    var normalDamageFrom: [PokemonType] {
+        return [.normal, .poison, .bug, .ghost, .steel, .water, .electric, .psychic, .ice, .dragon, .dark, .fairy]
+    }
+    
+    var doubleDamageFrom: [PokemonType] {
+        return [.flying, .rock, .fire]
+    }
+    
+}
+
 enum GhostType: TypeConformable {
     
     case secondType(type: PokemonType)
